24c139bd94859966dfbf3780a5410232286467bd
ryu.ryu <ryumiya51+53@gmail.com>
2024-04-30 02:40:15 +0900

    ヘッダー完成 24.04.30

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,24 @@
 
 jQuery(function ($) { // この中であればWordpressでも「$」が使用可能になる
+  //ドロワーメニュー
+  $(".js-hamburger, .js-sp-nav").click(function () {
+    if ($(".js-hamburger").hasClass('is-active')) {
+      $(".js-hamburger").removeClass("is-active");
+      $(".js-sp-nav").fadeOut(300);
+    } else {
+      $(".js-hamburger").addClass("is-active");
+      $(".js-sp-nav").fadeIn(300);
+    }
+  });
 
+  // 画面幅のサイズが変わったら
+  $(window).on('resize', function () {
+
+    // xマークを三マークにする（.js-hamburgerの要素にクラス名is-activeがあれば削除する）
+    $('.js-hamburger').removeClass('is-active');
+
+    // .js-sp-navを閉じる（.js-sp-navが表示されていれば非表示にする）
+    $('.js-sp-nav').fadeOut(300);
+  });
 
 });
