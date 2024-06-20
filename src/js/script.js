4fba7542f6f16a5675fa7cd2efd8d55b26d277a9
ryu.ryu <ryumiya51+53@gmail.com>
2024-06-20 19:06:23 +0900

    FAQページ完了 24.06.20

--- src/js/script.js
+++ src/js/script.js
@@ -271,12 +271,12 @@ jQuery(function ($) { // この中であればWordpressでも「$」が使用可
   });
 
   // トグル
-  $(".js-archive-toggle:first-child .js-archive-toggle__items").css(
+  $(".js-archive-toggle:first-child .js-archive-toggle__items, .js-accordion__item:first-child .js-accordion__content").css(
     "display",
     "block"
   );
-  $(".js-archive-toggle:first-child > .js-archive-toggle__title").addClass("is-open");
-  $(".js-archive-toggle__title").on("click", function () {
+  $(".js-archive-toggle:first-child > .js-archive-toggle__title, .js-accordion__item:first-child .js-accordion__title").addClass("is-open");
+  $(".js-archive-toggle__title, .js-accordion__title").on("click", function () {
     $(this).toggleClass("is-open");
     $(this).next().slideToggle(300);
   });
